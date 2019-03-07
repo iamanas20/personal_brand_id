e6bac37f256465bf5587e7721538f1099c4dd889
iamanas20 <anaslatique@gmail.com>
2019-03-07 10:30:39 +0100

    implemented navigation

---
+++ js/main.js
@@ -0,0 +1,39 @@
+function onLoad() {
+    var btns = document.getElementsByClassName("real-link");
+    var sections = document.getElementsByClassName("section");
+    
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+
+            var current = document.getElementsByClassName("active-nav-link");
+            current[0].className = current[0].className.replace(" active-nav-link", "");
+            this.className += " active-nav-link";
+
+            var current_section = document.getElementsByClassName("show-section");
+            
+            var num;
+
+            if (this.className.includes('1')) {
+                num = 1;
+            }
+            if (this.className.includes('2')) {
+                num = 2;
+            }
+            if (this.className.includes('3')) {
+                num = 3;
+            }
+            if (this.className.includes('4')) {
+                num = 4;
+            }
+            
+            current_section[0].className.replace(" show-section", "");
+            
+            current_section[0].className = current_section[0].className.replace(" show-section", "");
+            
+            sections[num - 1].className += " show-section";
+            
+        });
+
+        
+    }
+}
